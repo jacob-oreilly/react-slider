f3dca40f4d16e3a6ca52a8af8f05eb1fd7acbfc1
jacob-oreilly <jakeor45@gmail.com>
2021-12-21 21:18:53 -0800

    Initial commit added in some boiler plate code to make npm packages easier.

---
+++ src/lib/components/TestComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TestComponent = (props) => {
+    return (
+        <>
+            <h4>This is you starter component</h4>
+        </>
+    );
+}
+export default TestComponent;
