63a80bcc8eb6b9d878ef30fbc764c8136cd180fa
jacob-oreilly <jakeor45@gmail.com>
2022-01-29 22:25:41 -0800

    Added functionality to sliderControls so now it's updating to each slide when click the prev and next buttons

--- src/lib/components/slider.js
+++ src/lib/components/slider.js
@@ -6,14 +6,14 @@ import './slider.scss'
 const Slider = ({ data, height, width, showBullets, showControls, autoSlide, startIndex }) => {
     console.log(showBullets);
     const [activeSlide, setActiveSlide] = useState(startIndex ? startIndex : 0);
-
+    console.log(activeSlide);
     return (
         <>
             <h4>Slider</h4>
             <div className='slider'>
                 {data.map((element, index) => (<Slide key={index} data={element} showSlide={activeSlide === index ? 'active' : ''} />))}
             </div>
-            <SliderControls />
+            <SliderControls slideCount={data.length} activeSlide={activeSlide} setActiveSlide={setActiveSlide} />
         </>
     );
 }
--- src/lib/components/sliderControls.jsx
+++ src/lib/components/sliderControls.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-const SliderControls = ({ }) => {
+const SliderControls = ({ slideCount, activeSlide, setActiveSlide }) => {
+    let slideIndex = slideCount - 1;
+    function onControlClick(index) {
+        setActiveSlide(index);
+    }
     return (
         <div>
-            <button>Prev</button>
-            <button>Next</button>
+            <button onClick={() => onControlClick(activeSlide !== 0 ? (activeSlide - 1) : (activeSlide + slideIndex))}>Prev</button>
+            <button onClick={() => onControlClick(activeSlide !== slideIndex ? (activeSlide + 1) : 0)}>Next</button>
         </div>
     )
 }
